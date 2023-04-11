e2e20bdd75074d9e716f4c96c40b71e301dfce81
shyamal <shyamal.gayen@alshaya.com>
2023-04-11 14:17:24 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,27 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+} from 'react-native';
+
+function App() {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <SafeAreaView>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <Text>Hello</Text>
+    </SafeAreaView>
+  );
+}
+
+export default App;
